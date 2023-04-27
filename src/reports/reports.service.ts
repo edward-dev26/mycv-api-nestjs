3dce503f02c64b00b5900bc5fe0c57ec133bf976
eshvetsov <edward.shvetsov@eze.tech>
2023-04-27 14:03:15 +0300

    Add reports estimate

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { Report } from './report.entity';
 import { User } from '../users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -33,5 +34,21 @@ export class ReportsService {
     report.approved = approved;
 
     return this.reportsRepository.save(report);
+  }
+
+  async getEstimate({ make, model, year, miles, lat, lng }: GetEstimateDto) {
+    return this.reportsRepository
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(miles - :miles)', 'DESC')
+      .setParameters({ miles })
+      .limit(3)
+      .getRawOne();
   }
 }
